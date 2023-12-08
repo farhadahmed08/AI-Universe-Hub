a705e0d8e3f508ee53f39c3be6e8ff7bb4817491
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-09 02:55:45 +0800

    testing

--- js/AI.js
+++ js/AI.js
@@ -9,17 +9,30 @@ const loadData = async () =>{
 loadData()
 
 const displayData = (AI)=>{
-   console.log(AI)
+//    console.log(AI)
    const cardContainer = document.getElementById('card-container');
+//    const features = element.features;
+//    console.log(features)
+
    AI.forEach(element => {
         console.log(element);
+
+        // const elements = element.features;
+        // console.log(elements)
+        // elements.forEach(AiSingleFeatures => {
+        //     console.log(AiSingleFeatures)
+        // })
+
+
         const aiCard =document.createElement('div');
         aiCard.classList = 'card bg-green-100 p-4 shadow-xl';
         aiCard.innerHTML = `
         <figure><img src="${element.image}" alt="phones" /></figure>
         <div class="card-body">
         <h2 class="card-title">Features</h2>
-          <p> ${count++} ${element.features}</p>
+          <p>1. ${element.features[0]} </p>
+          <p>2. ${element.features[1]} </p>
+          <p>3. ${element.features[2]} </p>
           
           <hr>
           <h4 class="text-3xl">${element.name}</h4>
